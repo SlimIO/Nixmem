a8f1f39083415d06e7fd34e05d9b65ab2459e00e
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2018-10-06 18:42:12 +0200

    Fix freebsd version

--- nixmem.cpp
+++ nixmem.cpp
@@ -1,58 +1,50 @@
 #include "napi.h"
-// #include <sys/sysinfo.h>
-#include <sys/types.h>
-#include <vm/vm_param.h>
-#include <sys/sysctl.h>
+#include <errno.h>
+#include <sstream>
 #include <iostream>
-#include <string>
+#include <sys/types.h>
+#include <sys/sysctl.h>
 #include <sys/vmmeter.h>
 #include <sys/resource.h>
+#include <vm/vm_param.h>
 
 using namespace std;
 using namespace Napi;
 
-
 template<class T>
-T sysInfo(const int firstLevel, const int secondLevel ){
-    int name[2];
-    name[0] = firstLevel;
-    name[1] = secondLevel;
+T sysInfo(const int firstLevel, const int secondLevel) {
+    int name[2] = {firstLevel, secondLevel};
     T infoValue;
     size_t len = sizeof(infoValue);
     sysctl(name, (u_int) 2, &infoValue, &len, NULL, 0);
+
     return infoValue;
 }
 
-int sysInfo(const int firstLevel, const int secondLevel, void *info ){
-    int name[2];
-    name[0] = firstLevel;
-    name[1] = secondLevel;
+int sysInfo(const int firstLevel, const int secondLevel, void* info) {
+    int ret;
+    int name[2] = {firstLevel, secondLevel};
     size_t len = sizeof(info);
-    int status = sysctl(name, (u_int) 2, &info, &len, NULL, 0);
-    return status;
+
+    ret = sysctl(name, (u_int) 2, NULL, &len, NULL, 0);
+    if (ret != -1) {
+        return ret;
+    }
+    info = malloc(len);
+
+    return sysctl(name, (u_int) 2, &info, &len, NULL, 0);
 }
 
 Value getSysInfo(const CallbackInfo& info){
     Env env = info.Env();
-
-    unsigned int  nCPU = sysInfo<unsigned int>(CTL_HW, HW_NCPU);
-    long byteOrder = sysInfo<long>(CTL_HW, HW_BYTEORDER);
-    long physMem = sysInfo<long>(CTL_HW, HW_PHYSMEM);
-    long userMem = sysInfo<long>(CTL_HW, HW_USERMEM);
-    long pageSize = sysInfo<long>(CTL_HW, HW_PAGESIZE);
-    long realMem = sysInfo<long>(CTL_HW, HW_REALMEM);
-
-    // struct loadavg load_info;
-    // int statusLoadavg = sysInfo(CTL_VM, VM_LOADAVG, &load_info);
     struct vmtotal vm_info;
-    int statusVmtotal = sysInfo(CTL_VM, VM_TOTAL, &vm_info);
-    bool swapping_enable = sysInfo<bool>(CTL_VM, VM_SWAPPING_ENABLED);
-    long vFreeMin = sysInfo<long>(CTL_VM, VM_V_FREE_MIN);
-    long vFreeReserved = sysInfo<long>(CTL_VM, VM_V_FREE_RESERVED);
-    long vFreeTarget = sysInfo<long>(CTL_VM, VM_V_FREE_TARGET);
-    long vInactiveTarget = sysInfo<long>(CTL_VM, VM_V_INACTIVE_TARGET);
-    long vPageoutFreeMin = sysInfo<long>(CTL_VM, VM_V_PAGEOUT_FREE_MIN);
-    // long vOvercommit = sysInfo<long>(CTL_VM, VM_OVERCOMMIT);
+
+    if (sysInfo(CTL_VM, VM_TOTAL, &vm_info) == -1) {
+        stringstream err;
+        err << "sysctl failed for CTL_VM.VM_TOTAL: " << errno << endl;
+        Error::New(env, err.str()).ThrowAsJavaScriptException();
+        return env.Null();
+    }
 
     Object vmtotal = Object::New(env);
     vmtotal.Set("t_vm", vm_info.t_vm);
@@ -71,23 +63,22 @@ Value getSysInfo(const CallbackInfo& info){
     vmtotal.Set("t_sw", vm_info.t_sw);
 
     Object ret = Object::New(env);
-    ret.Set("nCPU", nCPU);
-    ret.Set("byteOrder", byteOrder);
-    ret.Set("physMem", physMem);
-    ret.Set("userMem", userMem);
-    ret.Set("pageSize", pageSize);
-    ret.Set("realMem", realMem);
+    ret.Set("nCPU", sysInfo<unsigned int>(CTL_HW, HW_NCPU));
+    ret.Set("byteOrder", sysInfo<long>(CTL_HW, HW_BYTEORDER));
+    ret.Set("physMem", sysInfo<long>(CTL_HW, HW_PHYSMEM));
+    ret.Set("userMem", sysInfo<long>(CTL_HW, HW_USERMEM));
+    ret.Set("pageSize", sysInfo<long>(CTL_HW, HW_PAGESIZE));
+    ret.Set("realMem", sysInfo<long>(CTL_HW, HW_REALMEM));
     ret.Set("vmtotal", vmtotal);
-    ret.Set("swapping_enable", swapping_enable);
-    ret.Set("vFreeMin", vFreeMin);
-    ret.Set("vFreeReserved", vFreeReserved);
-    ret.Set("vFreeTarget", vFreeTarget);
-    ret.Set("vInactiveTarget", vInactiveTarget);
-    ret.Set("vPageoutFreeMin", vPageoutFreeMin);
+    ret.Set("swapping_enable", sysInfo<bool>(CTL_VM, VM_SWAPPING_ENABLED));
+    ret.Set("vFreeMin", sysInfo<long>(CTL_VM, VM_V_FREE_MIN));
+    ret.Set("vFreeReserved", sysInfo<long>(CTL_VM, VM_V_FREE_RESERVED));
+    ret.Set("vFreeTarget", sysInfo<long>(CTL_VM, VM_V_FREE_TARGET));
+    ret.Set("vInactiveTarget", sysInfo<long>(CTL_VM, VM_V_INACTIVE_TARGET));
+    ret.Set("vPageoutFreeMin", sysInfo<long>(CTL_VM, VM_V_PAGEOUT_FREE_MIN));
 
     return ret;
 }
-
 
 /*
  * Initialize Node.JS Binding exports
@@ -96,8 +87,9 @@ Value getSysInfo(const CallbackInfo& info){
  */
 Object Init(Env env, Object exports) {
     exports.Set("getSysInfo", Function::New(env, getSysInfo));
+
     return exports;
 }
 
 // Export
-NODE_API_MODULE(Winservices, Init)
+NODE_API_MODULE(Nixmem, Init)
