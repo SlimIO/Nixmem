307ee8d6829f52214a26f71e532fefb0ff88afd5
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2018-12-05 13:05:13 +0100

    chore(package): update node-adddon-api and replace bindings

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @namespace nixmem
  * @desc UNIX Memory
  */
-const nixmem = require("bindings")("nixmem.node");
+const nixmem = require("node-gyp-build")(__dirname);
 
 console.time("sysinfo");
 console.log(nixmem.getSysInfo());
