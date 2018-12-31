d733e7574694e8c417b28a6ae07fb557699ac2f4
root <root@DESKTOP-UHHC0KF.localdomain>
2018-12-31 01:59:19 +0100

    chore: work on MemInfo interface

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,28 @@
 declare namespace Nixmem {
+    interface MemInfo {
+        memTotal: number;
+        memFree: number;
+        memShared: number;
+        memAvailable: number;
+        swapCached: number;
+        swapTotal: number;
+        swapFree: number;
+        anonPages: number;
+        pageTables: number;
+        shmemHugePages: number;
+        shmemPmdMapped: number;
+        hugePagesTotal: number;
+        hugePagesFree: number;
+        hugePagesRsvd: number;
+        hugePagesSurp: number;
+        hugePageSize: number;
+        commitLimit: number;
+        vMallocTotal: number;
+        vMallocUsed: number;
+        vMallocChunk: number;
+    }
 
+    export function getSysInfo(): MemInfo;
 }
 
 export as namespace Nixmem;
